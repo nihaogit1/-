6f883f9a800d75b25e7792a87e29c8f1b16de6a7
s15340888379 <1559077212@qq.com>
2020-08-20 07:43:05 +0800

    -===

--- src/context/usecontext.ts
+++
@@ -1,9 +0,0 @@
-import {useContext} from "react"
-
-import storeContext from "../context/Storecontext"
-const useStore = () =>{
-    return useContext(storeContext)
-}
-
-console.log("====")
-export default useStore
